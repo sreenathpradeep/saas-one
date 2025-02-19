28ffa24674dae68db2b80baba724c6bc81ee83b4
sreenath”CORRECT_EMAIL=sreenathpradeep@gmail.com” <>
2025-02-20 03:03:58 +0530

    new button component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
   return (
     <main>
       <h1>Collect customer feedback to build better products</h1>
       <div>Create a feedback board in minutes</div>
-      <button>Get Started</button>
+      <ButtonLogin></ButtonLogin>
     </main>
   );
 }
