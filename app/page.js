d16888205a2a6b33e84efb3b286764369a450e0a
sreenath”CORRECT_EMAIL=sreenathpradeep@gmail.com” <>
2025-02-20 01:08:29 +0530

    added a button

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main>
       <h1>Collect customer feedback to build better products</h1>
       <div>Create a feedback board in minutes</div>
+      <button>Get Started</button>
     </main>
   );
 }
