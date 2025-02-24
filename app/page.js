46b51178befb1e150f585fe4be85377ce013d9ab
sreenath”CORRECT_EMAIL=sreenathpradeep@gmail.com” <>
2025-02-25 01:22:34 +0530

    modified hero section with tailwind classes

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
- 
- // variables
+  // variables
   const isLoggedIn = true;
   const user = "Sreenath";
 
   //main app
   return (
     <main>
-      <h1 className="title">Collect customer feedback to build better products</h1>
-      <div className="title">Create a feedback board in minutes</div>
-      <ButtonLogin isLoggedIn={isLoggedIn} name={user} />
+      <section className="px-8 text-center py-32 max-w-3xl mx-auto">
+        <h1 className="text-4xl font-extrabold mb-6">
+          Collect customer feedback to build better products
+        </h1>
+        <div className="opacity-90 mb-10">
+          Create a feedback board in minutes, prioritize features, and build products your customers will love.
+        </div>
+        <ButtonLogin isLoggedIn={isLoggedIn} name={user} />
+      </section>
     </main>
   );
 }
